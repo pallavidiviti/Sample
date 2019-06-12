0cdef5089c4c563e9d45358edb44e569172f94ee
Pallavi Diviti <pallavidiviti@Pallavis-MacBook-Air.local>
2019-06-12 17:48:32 +0530

    Initial Commit

---
+++ PallaviFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PallaviFirstApp
+//
+//  Created by Pallavi Diviti on 12/06/19.
+//  Copyright © 2019 Pallavi Diviti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
