d8377592e6ca85ba38a63dbe66efe6e40ab30c30
pallavi <pallavi.diviti@gmail.com>
2019-06-21 12:55:48 +0530

    Added Print Stmt

--- PallaviFirstApp/ViewController.swift
+++ PallaviFirstApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        print("Sample Project Test with Git Hub")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
